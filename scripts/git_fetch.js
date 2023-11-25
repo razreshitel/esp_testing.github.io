db99424be7fc3b7ea261c88a7fb4c16d793a48ba
razreshitel <sashok2312@gmail.com>
2023-11-26 00:18:39 +0300

    -

--- scripts/git_fetch.js
+++ scripts/git_fetch.js
@@ -1,14 +1,22 @@
-document.getElementById('scripts/update-repo-btn').addEventListener('click', function() {
-    var password = prompt("Please enter the password:");
-    if (password != null) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "scripts/update_repo.php", true);
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhr.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                alert(this.responseText);
+document.getElementById('update-repo-btn').addEventListener('click', function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "scripts/last_update_time.php", true); // Request to get the last update time
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var lastUpdateTime = this.responseText; // Response contains the last update time
+            var password = prompt("Please enter the password:\nLast update was on " + lastUpdateTime);
+            if (password != null) {
+                var updateXhr = new XMLHttpRequest();
+                updateXhr.open("POST", "scripts/update_repo.php", true);
+                updateXhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                updateXhr.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        alert(this.responseText);
+                    }
+                };
+                updateXhr.send("password=" + password);
             }
-        };
-        xhr.send("password=" + password);
-    }
+        }
+    };
+    xhr.send();
 });
--- scripts/update_repo.php
+++ scripts/update_repo.php
@@ -1,14 +1,25 @@
 <?php
 $stored_hash = 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3';
 
+// Handle POST request for updating the repository
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (hash('sha256', $_POST['password']) === $stored_hash) {
         echo "Starting update...\n";
         $output = shell_exec('git fetch --all && git reset --hard origin/main && git clean -fd && echo "Update completed." 2>&1');
-        file_put_contents('../git.log', date("Y-m-d H:i:s") . " - ESP - Update executed\n", FILE_APPEND);
+        file_put_contents('../../git.log', date("Y-m-d H:i:s") . " - ESP - Update executed\n", FILE_APPEND);
         echo nl2br($output);
     } else {
         echo "Incorrect Password";
+    }
+} else {
+    // Handle GET request to retrieve the last update time
+    $logFilePath = '../../git.log'; // Adjust the path as needed
+
+    if (file_exists($logFilePath)) {
+        $lastLine = `tail -n 1 $logFilePath`; // Gets the last line of the git.log file
+        echo $lastLine;
+    } else {
+        echo "No update information available.";
     }
 }
 ?>
