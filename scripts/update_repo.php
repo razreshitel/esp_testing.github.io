4fa31f7f905a01471001608d555803df20d6d7d3
razreshitel <sashok2312@gmail.com>
2023-11-26 04:01:41 +0300

    -

--- scripts/update_repo.php
+++ scripts/update_repo.php
@@ -5,6 +5,7 @@ $stored_hash = 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (hash('sha256', $_POST['password']) === $stored_hash) {
         echo "Starting update...\n";
+        $output = shell_exec('git fetch --all && git reset --hard origin/main && git clean -fd && echo "Update completed." 2>&1');
     } else {
         echo "Incorrect Password";
     }
