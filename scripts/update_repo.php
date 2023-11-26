3783ad54be0be0891305cef9dfeaa8b8ac270fd9
razreshitel <sashok2312@gmail.com>
2023-11-26 15:03:42 +0300

    -

--- scripts/update_repo.php
+++ scripts/update_repo.php
@@ -4,7 +4,7 @@ $stored_hash = 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (hash('sha256', $_POST['password']) === $stored_hash) {
         echo "Starting update...\n";
-        $output = shell_exec('git fetch --all && git reset --hard origin/main && git clean -fd 2>&1');
+        $output = shell_exec('cd /home/pi/github/esp_testing.io && git fetch --all && git reset --hard origin/main && git clean -fd 2>&1');
         file_put_contents('../../git.log', date("Y-m-d H:i:s") . " - Update executed\n", FILE_APPEND);
         echo nl2br($output . "\nUpdate completed.");
     } else {
